48208036707b55f16014c4f7d3e9848b014623d9
Evil-crow <Evilcrow486@gmail.com>
2019-03-02 22:39:31 +0800

    class log_buffer will be used to store log message

---
+++ src/logger/log_buffer.cc
@@ -0,0 +1,60 @@
+/**
+ * Created by Crow on 2/1/19.
+ * Copyright (c) 2019 Crow All rights reserved.
+ * @author Crow
+ * @brief  
+ */
+
+#include "log_buffer.h"
+
+#include <cstring>
+#include <memory>
+
+namespace log {
+
+log_buffer::log_buffer(size_t size)
+    : index_(0),
+      size_(size > max_data_size ? max_data_size : size),
+      data_(std::make_unique<char []>(size_))
+{
+  ;
+}
+
+log_buffer::log_buffer() : log_buffer(max_data_size) { ; }
+
+bool log_buffer::append(const char *str, size_t len)
+{
+  if (available() > len) {
+    ::memcpy(data_.get(), str, len);
+    return true;
+  }
+
+  return false;
+}
+
+size_t log_buffer::available() const
+{
+  return size_ - index_;
+}
+
+size_t log_buffer::size() const
+{
+  return size_;
+}
+
+size_t log_buffer::count() const
+{
+  return index_;
+}
+
+char *log_buffer::data() const
+{
+  return data_.get();
+}
+
+void log_buffer::reset()
+{
+  ::memset(data_.get(), 0, sizeof(size_));
+}
+
+}
