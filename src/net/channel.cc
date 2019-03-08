2966549469dd3adcb6c4f9a1786339f132716f98
Evil-crow <Evilcrow486@gmail.com>
2019-03-08 21:20:33 +0800

    Class Channel for Event Handle

---
+++ src/net/channel.cc
@@ -0,0 +1,59 @@
+/**
+ * Created by Crow on 3/7/19.
+ * Copyright (c) 2019 Crow All rights reserved.
+ * @author Crow 
+ */
+
+#include "net/channel.h"
+
+#include "net/event_loop.h"
+
+namespace levelnet {
+
+Channel::Channel(EventLoop *loop, int fd)
+  : loop_(loop), fd_(fd) { ; }
+
+inline void Channel::SetReadCallback(levelnet::Channel::EventCallback &&cb)  { read_callback_ = cb; }
+inline void Channel::SetWriteCallback(levelnet::Channel::EventCallback &&cb) { write_callback_ = cb; }
+inline void Channel::SetCloseCallback(levelnet::Channel::EventCallback &&cb) { close_callback_ = cb; }
+inline void Channel::SetErrorCallback(levelnet::Channel::EventCallback &&cb) { err_callback_ = cb; }
+
+void Channel::SetFlags(ChannelEvent event, bool flag) {
+  flags_ = flag ? (flags_ | event) : (flags_ & ~event);
+}
+
+void Channel::ClearFlags() {
+  flags_ = 0;
+}
+
+void Channel::SetEvents(unsigned int events) {
+  revents_ = events;
+}
+
+void Channel::HandleEvent() {
+  event_handling_ = true;
+  if (revents_ & ChannelEvent::ERR) {
+    if (err_callback_)
+      err_callback_();
+  } else if (revents_ & ChannelEvent::HUP) {
+    if (close_callback_)
+      close_callback_();
+  } else if (revents_ & ChannelEvent::R) {
+    if (read_callback_)
+      read_callback_();
+  } else if (revents_ & ChannelEvent::W) {
+    if (write_callback_)
+      write_callback_();
+  }
+  event_handling_ = false;
+}
+
+int Channel::Fd() const {
+  return fd_;
+}
+
+unsigned int Channel::Flags() const {
+  return flags_;
+}
+
+}
---
+++ src/net/channel.h
@@ -0,0 +1,60 @@
+/**
+ * Created by Crow on 3/7/19.
+ * Copyright (c) 2019 Crow All rights reserved.
+ * @author Crow 
+ */
+
+#ifndef LEVELNET_CHANNEL_H
+#define LEVELNET_CHANNEL_H
+
+#include <sys/epoll.h>
+#include <functional>
+
+namespace levelnet {
+
+class EventLoop;
+
+enum ChannelEvent {
+  R = EPOLLIN,
+  W = EPOLLOUT,
+  ET = EPOLLET,
+  ERR = EPOLLERR,
+  HUP = EPOLLRDHUP,
+};
+
+class Channel {
+ public:
+  using EventCallback = std::function<void()>;
+
+  Channel(EventLoop *loop,int fd);
+  ~Channel() = default;
+
+  void HandleEvent();
+  void SetFlags(ChannelEvent event, bool flag);
+  void ClearFlags();
+  void SetEvents(unsigned int events);
+  void SetReadCallback(EventCallback &&cb);
+  void SetWriteCallback(EventCallback &&cb);
+  void SetCloseCallback(EventCallback &&cb);
+  void SetErrorCallback(EventCallback &&cb);
+
+  int Fd() const;
+  unsigned int Flags() const;
+
+ private:
+  EventLoop *loop_;
+  int fd_;
+
+  unsigned int flags_;
+  unsigned int revents_;
+  bool event_handling_;
+
+  EventCallback read_callback_;
+  EventCallback write_callback_;
+  EventCallback err_callback_;
+  EventCallback close_callback_;
+};
+
+}
+
+#endif //LEVELNET_CHANNEL_H
